47d87a58d3cd06fee17780ed427f8fdce196281a
Jackson Kearl <jakearl@microsoft.com>
2020-02-13 23:30:56 -0800

    Clean up old stuff

--- myapp/typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/body-parser/index.d.ts" />
-/// <reference path="browser/ambient/express-serve-static-core/index.d.ts" />
-/// <reference path="browser/ambient/express/index.d.ts" />
-/// <reference path="browser/ambient/mime/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/serve-static/index.d.ts" />
